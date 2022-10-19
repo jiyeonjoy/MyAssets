1280eabcc6064b03fb3202b28c13564a711e7cd9
최지연 <jyc@joonggonara.co.kr>
2022-10-19 18:16:23 +0900

    Initial Commit

---
+++ MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by Jiyeon Choi on 2022. 10. 19..
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
