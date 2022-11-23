efc68542a130b4bb5c97bb9bd62d91c6c05e8cc8
최지연 <choi02647@naver.com>
2022-11-23 17:33:02 +0900

    added bannerCard

---
+++ MyAssets/Asset/SubComponents/BannerView/SubComponents/BannerCard.swift
@@ -0,0 +1,31 @@
+//
+//  BannerCard.swift
+//  MyAssets
+//
+//  Created by Jiyeon Choi on 2022. 11. 23..
+//
+
+import SwiftUI
+
+struct BannerCard: View {
+    var banner: AssetBanner
+    
+    var body: some View {
+        Color(banner.backgroundColor)
+            .overlay(
+                VStack {
+                    Text(banner.title)
+                        .font(.title)
+                    Text(banner.description)
+                        .font(.subheadline)
+                }
+            )
+    }
+}
+
+struct BannerCard_Previews: PreviewProvider {
+    static var previews: some View {
+        BannerCard(banner: AssetBanner(title: "공지사항", description: "추가된 공지사항을 확인하세요", backgroundColor: .red))
+            .aspectRatio(5/2, contentMode: .fit)
+    }
+}
