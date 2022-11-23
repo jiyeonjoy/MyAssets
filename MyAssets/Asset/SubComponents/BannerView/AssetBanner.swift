2667a6c92d046b35a63bd64fc217c272d9ac7132
최지연 <choi02647@naver.com>
2022-11-23 17:32:12 +0900

    added bannerView, PageViewControll, PageControll

---
+++ MyAssets/Asset/SubComponents/BannerView/AssetBanner.swift
@@ -0,0 +1,14 @@
+//
+//  AssetBanner.swift
+//  MyAssets
+//
+//  Created by Jiyeon Choi on 2022. 11. 23..
+//
+
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
